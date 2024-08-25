b529b3c89703aa4b3e98608e084ecbb60b524ac2
Ayesha-chaudhry <ashijutt069@gmail.com>
2024-08-25 06:00:41 +0500

    text-white

--- src/app/components/views/Spy/Spy.tsx
+++ src/app/components/views/Spy/Spy.tsx
@@ -3,7 +3,7 @@ import Button from '../../Button'
 
 export const Spy = () => {
   return (
-    <div className='bg-[#4335DE] text-center flex flex-col gap-8 py-14'>
+    <div className='bg-[#4335DE] text-center flex flex-col gap-8 py-14 text-white'>
         <h1 className='text-[34px] font-extrabold'>Is someone spying on your phone?</h1>
         <p>Find out with Certo</p>
         <div className="md:flex gap-12 mx-auto">
